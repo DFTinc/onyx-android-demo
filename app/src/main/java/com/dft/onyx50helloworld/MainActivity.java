95f1becaa749def8c06915f1a189ae1976bf543e
cdwheatley <cwheatley@diamondfortress.com>
2018-02-25 21:56:09 -0600

    Initial commit.

---
+++ app/src/main/java/com/dft/onyx50helloworld/MainActivity.java
@@ -0,0 +1,129 @@
+package com.dft.onyx50helloworld;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dft.onyxcamera.config.Onyx;
+import com.dft.onyxcamera.config.OnyxConfiguration;
+import com.dft.onyxcamera.config.OnyxConfigurationBuilder;
+import com.dft.onyxcamera.config.OnyxResult;
+
+import static com.dft.onyx50helloworld.ValuesUtil.*;
+
+public class MainActivity extends AppCompatActivity {
+    private Activity activity;
+    private Onyx onyx = null;
+    private Button startOnyxButton;
+    private ImageView rawImageView;
+    private ImageView processedImageView;
+    private ImageView enhancedImageView;
+    private TextView livenessResultTextView;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setupUI();
+        setupOnyx(this);
+    }
+
+    private void setupOnyx(final Activity activity) {
+        // Create an OnyxConfigurationBuilder and configure it with desired options
+        OnyxConfigurationBuilder onyxConfigurationBuilder = new OnyxConfigurationBuilder()
+            .setActivity(activity)
+            .setLicenseKey(getResources().getString(R.string.onyx_license))
+            .setReturnRawBitmap(getReturnRawBitmap(this))
+            .setReturnProcessedBitmap(getReturnProcessedBitmap(this))
+            .setReturnEnhancedBitmap(getReturnEnhancedBitmap(this))
+            .setReturnWSQ(getReturnWSQ(this))
+            .setReturnFingerprintTemplate(getReturnFingerprintTemplate(this))
+            .setShowLoadingSpinner(getShowLoadingSpinner(this))
+            .setUseOnyxLive(getUseOnyxLive(this))
+            .setUseFlash(getUseFlash(this))
+            .setShouldSegment(getShouldSegment(this))
+            .setImageRotation(getImageRotation(this))
+            .setReticleOrientation(getReticleOrientation(this))
+            .setCropSize(getCropSizeWidth(this), getCropSizeHeight(this))
+            .setCropFactor(getCropFactor(this))
+            .setReticleScale(getReticleScale(this))
+            .setLayoutPreference(getLayoutPreference(this))
+            .setSuccessCallback(new OnyxConfiguration.SuccessCallback() {
+                @Override
+                public void onSuccess(OnyxResult onyxResult) {
+                    displayResults(onyxResult);
+                }
+            })
+            .setErrorCallback(new OnyxConfiguration.ErrorCallback() {
+                @Override
+                public void onError(Error error, String errorMessage, Exception exception) {
+                    Log.e("MainActivity", errorMessage);
+                }
+            })
+            .setOnyxCallback(new OnyxConfiguration.OnyxCallback() {
+
+                @Override
+                public void onConfigured(Onyx configuredOnyx) {
+                    onyx = configuredOnyx;
+                    startOnyxButton.setEnabled(true);
+                }
+            });
+        // Reticle Angle overrides Reticle Orientation so have to set this separately
+        if (getReticleAngle(this) != null) {
+            onyxConfigurationBuilder.setReticleAngle(getReticleAngle(this));
+        }
+        // Finally, build the OnyxConfiguration
+        onyxConfigurationBuilder.buildOnyxConfiguration();
+    }
+
+    private void startOnyx() {
+        onyx.create();
+        onyx.capture();
+    }
+
+    private void displayResults(OnyxResult onyxResult) {
+        rawImageView.setImageDrawable(null);
+        processedImageView.setImageDrawable(null);
+        enhancedImageView.setImageDrawable(null);
+        if (onyxResult.getRawFingerprintBitmap() != null) {
+            rawImageView.setImageBitmap(onyxResult.getRawFingerprintBitmap());
+        }
+        if (onyxResult.getProcessedFingerprintBitmap() != null) {
+            processedImageView.setImageBitmap(onyxResult.getProcessedFingerprintBitmap());
+        }
+        if (onyxResult.getEnhancedFingerprintBitmap() != null) {
+            enhancedImageView.setImageBitmap(onyxResult.getEnhancedFingerprintBitmap());
+        }
+        if (onyxResult.getMetrics() != null) {
+//            livenessResultTextView.setText(onyxResult.getMetrics().getLiveness());
+        }
+    }
+
+    private void setupUI() {
+        activity = this;
+        setContentView(R.layout.activity_main);
+        rawImageView = findViewById(R.id.rawImageView);
+        processedImageView = findViewById(R.id.processedImageView);
+        enhancedImageView = findViewById(R.id.enhancedImageView);
+        startOnyxButton = findViewById(R.id.start_onyx);
+        startOnyxButton.setEnabled(false);
+        startOnyxButton.bringToFront();
+        startOnyxButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startOnyx();
+            }
+        });
+        Button refreshConfigButton = findViewById(R.id.refresh_config);
+        refreshConfigButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setupOnyx(activity);
+                startOnyxButton.setEnabled(false);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/dft/onyx50helloworld/ValuesUtil.java
@@ -0,0 +1,109 @@
+package com.dft.onyx50helloworld;
+
+import android.app.Activity;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.dft.onyxcamera.ui.OnyxFragment;
+import com.dft.onyxcamera.ui.reticles.Reticle;
+
+/**
+ * Created by BigWheat on 2/10/2018.
+ */
+
+public class ValuesUtil {
+
+    public static boolean getReturnRawBitmap(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.returnRawBitmap)).isChecked();
+    }
+
+    public static boolean getReturnProcessedBitmap(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.returnProcessedBitmap)).isChecked();
+    }
+
+    public static boolean getReturnEnhancedBitmap(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.returnEnhancedBitmap)).isChecked();
+    }
+
+    public static boolean getReturnWSQ(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.returnWSQ)).isChecked();
+    }
+
+    public static boolean getReturnFingerprintTemplate(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.returnFingerprintTemplate)).isChecked();
+    }
+
+    public static boolean getShowLoadingSpinner(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.showLoadingSpinner)).isChecked();
+    }
+
+    public static boolean getUseOnyxLive(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.useOnyxLive)).isChecked();
+    }
+
+    public static boolean getUseFlash(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.useFlash)).isChecked();
+    }
+
+    public static boolean getShouldSegment(Activity a) {
+        return ((CheckBox) a.findViewById(R.id.shouldSegment)).isChecked();
+    }
+
+    public static Integer getImageRotation(Activity a) {
+        return Integer.valueOf(((Spinner) a.findViewById(R.id.imageRotation)).getSelectedItem().toString());
+    }
+
+    public static Reticle.Orientation getReticleOrientation(Activity a) {
+        return (((Spinner) a.findViewById(R.id.reticleOrientationSpinner)).getSelectedItem().toString().equalsIgnoreCase(Reticle.Orientation.LEFT.toString())) ?
+                Reticle.Orientation.LEFT : Reticle.Orientation.RIGHT;
+    }
+
+    public static Float getReticleAngle(Activity a) {
+        EditText reticleAngleEditText = a.findViewById(R.id.reticleAngleEditText);
+        if (reticleAngleEditText.getText() != null && !reticleAngleEditText.getText().toString().equals("")) {
+            return Float.valueOf(reticleAngleEditText.getText().toString());
+        }
+        return null;
+    }
+
+    public static Double getCropSizeWidth(Activity a) {
+        if (((CheckBox) a.findViewById(R.id.cropSize)).isChecked()) {
+            return Double.valueOf(((EditText) a.findViewById(R.id.widthEditText)).getText().toString());
+        } else {
+            return 512.0;
+        }
+    }
+
+    public static Double getCropSizeHeight(Activity a) {
+        if (((CheckBox) a.findViewById(R.id.cropSize)).isChecked()) {
+            return Double.valueOf(((EditText) a.findViewById(R.id.heightEditText)).getText().toString());
+        } else {
+            return 300.0;
+        }
+    }
+
+    public static Float getCropFactor(Activity a) {
+        if (((CheckBox) a.findViewById(R.id.cropFactor)).isChecked()) {
+            return Float.valueOf(((EditText) a.findViewById(R.id.cropFactorEditText)).getText().toString());
+        } else {
+            return 1.0f;
+        }
+    }
+
+    public static Float getReticleScale(Activity a) {
+        if (((CheckBox) a.findViewById(R.id.reticleScale)).isChecked()) {
+            return Float.valueOf(((EditText) a.findViewById(R.id.reticleScaleEditText)).getText().toString());
+        } else {
+            return 1.0f;
+        }
+    }
+
+    public static OnyxFragment.LayoutPreference getLayoutPreference(Activity a) {
+        if (((CheckBox) a.findViewById(R.id.layoutPreferenceFullScreen)).isChecked()) {
+            return OnyxFragment.LayoutPreference.FULL;
+        } else {
+            return OnyxFragment.LayoutPreference.UPPER_THIRD;
+        }
+    }
+}
